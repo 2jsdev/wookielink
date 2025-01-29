055fa1029ac807e6156817167b8c4e9fbfe937d7
Jordy Morales <dev.morales.jordy@gmail.com>
2025-01-29 10:09:03 -0400

    feat: enhance authentication flow; redirect authenticated users from login to admin dashboard

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,23 +5,26 @@ import { authConfig } from '@/@core/infra/auth';
 const { auth } = NextAuth(authConfig);
 
 export const LOGIN_ROUTE = '/login';
+export const ADMIN_DASHBOARD = '/admin';
 
 export default auth((req) => {
   const { nextUrl } = req;
 
+  const isAuthenticated = !!req.auth;
+  const isLoginPage = nextUrl.pathname === LOGIN_ROUTE;
   const isPrivateRoute = nextUrl.pathname.startsWith('/admin');
 
-  if (isPrivateRoute) {
-    const isAuthenticated = !!req.auth;
-    if (!isAuthenticated) {
-      return NextResponse.redirect(new URL(LOGIN_ROUTE, nextUrl));
-    }
-    return NextResponse.next();
+  if (isAuthenticated && isLoginPage) {
+    return NextResponse.redirect(new URL(ADMIN_DASHBOARD, nextUrl));
+  }
+
+  if (isPrivateRoute && !isAuthenticated) {
+    return NextResponse.redirect(new URL(LOGIN_ROUTE, nextUrl));
   }
 
   return NextResponse.next();
 });
 
 export const config = {
-  matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)'], // Match all relevant routes
+  matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)'],
 };
