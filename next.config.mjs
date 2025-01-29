3a0730c32c39f4c603e47a6abad9ef560978b788
Jordy Morales <dev.morales.jordy@gmail.com>
2025-01-29 14:40:53 -0400

    feat: update Next.js configuration; enable standalone output and modify image remote patterns

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'standalone',
   images: {
-    domains: ['lh3.googleusercontent.com', 'minio-api.2jsdev.me'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'minio-api.2jsdev.me',
+      },
+    ],
   },
 };
 
