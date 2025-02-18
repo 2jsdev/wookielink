00c84d669c88303224e5872672810ec8dea9ba91
Jordy Morales <dev.morales.jordy@gmail.com>
2025-02-18 08:02:22 -0400

    feat: enhance ShareLinkModal layout and improve accessibility with dynamic styling

--- src/components/custom/ShareLinkModal.tsx
+++ src/components/custom/ShareLinkModal.tsx
@@ -24,9 +24,10 @@ import {
 } from 'react-share';
 import { Card } from '@/components/ui/card';
 import { ScrollArea, ScrollBar } from '@/components/ui/scroll-area';
-import { Link as LinkIcon, MoreHorizontal } from 'lucide-react';
+import { Check, Link as LinkIcon, Share } from 'lucide-react';
 import { Link as LinkType } from '@/interfaces/Link';
 import Link from 'next/link';
+import { cn } from '@/lib/utils';
 
 interface ShareLinkModalProps {
   link: LinkType;
@@ -87,106 +88,139 @@ export default function ShareLinkModal({
           className="shadow-none border-none flex items-center justify-center text-white cursor-pointer"
           onClick={() => window.open(link.url!, '_blank')}
         >
-          <div className="w-full md:w-[327px] bg-primary rounded-lg p-6 shadow-low-elevation-light hover:scale-[1.01] hover:shadow-max-elevation-light">
-            {link.ogData && (
-              <div className="flex flex-col items-center justify-center gap-4">
-                {link.ogData?.ogImage?.[0]?.url && (
-                  <div className="h-[120px] w-[120px] flex items-center justify-center">
-                    <Image
-                      src={link.ogData?.ogImage?.[0]?.url}
-                      alt={link.ogData?.ogTitle || 'Thumbnail'}
-                      width={80}
-                      height={80}
-                      className="h-full w-full rounded-sm object-cover"
-                    />
-                  </div>
-                )}
-
-                <div className="flex flex-col items-center justify-center gap-2">
-                  <h3 className="text-[20px] font-extrabold text-center leading-normal text-balance">
-                    {link.ogData?.ogTitle || 'Untitled'}
-                  </h3>
-                  <p className="text-xs text-center text-gray-300 truncate">
-                    {link.url!}
-                  </p>
+          <div
+            className={cn(
+              'w-full md:w-[327px] rounded-lg p-6 shadow-low-elevation-light hover:scale-[1.01] hover:shadow-max-elevation-light',
+              {
+                'bg-primary text-white': link.ogData?.ogTitle,
+                'bg-gray-100 text-black': !link.ogData?.ogTitle,
+              }
+            )}
+          >
+            <div className="flex flex-col items-center justify-center gap-4">
+              {link.ogData?.ogImage?.[0]?.url && (
+                <div className="h-[120px] w-[120px] flex items-center justify-center">
+                  <Image
+                    src={link.ogData?.ogImage?.[0]?.url}
+                    alt={link.ogData?.ogTitle || 'Thumbnail'}
+                    width={80}
+                    height={80}
+                    className="h-full w-full rounded-sm object-cover"
+                  />
                 </div>
+              )}
 
+              <div className="flex flex-col items-center justify-center gap-2">
+                <h3 className="text-[20px] font-extrabold text-center leading-normal text-balance">
+                  {link.ogData?.ogTitle || link.title}
+                </h3>
+                <p className="text-xs text-center text-gray-500 truncate">
+                  {link.url}
+                </p>
+              </div>
+
+              {link.ogData?.ogDescription && (
                 <p className="text-xs text-center break-words text-balance line-clamp-3">
                   {link.ogData?.ogDescription}
                 </p>
-              </div>
-            )}
+              )}
+            </div>
           </div>
         </Card>
 
         <ScrollArea className="w-full">
-          <div className="flex space-x-3 my-4">
+          <div className="flex space-x-2 my-4">
             <div className="flex-shrink-0">
               <button
                 onClick={handleCopyLink}
-                className="flex flex-col items-center gap-1"
+                className="flex flex-col items-center gap-1 w-16"
               >
                 <div className="w-12 h-12 rounded-full bg-gray-100 hover:bg-gray-200 flex items-center justify-center">
-                  <LinkIcon className="w-5 h-5" />
+                  {copiedLink ? (
+                    <Check className="w-5 h-5 text-green-500" />
+                  ) : (
+                    <LinkIcon className="w-5 h-5" />
+                  )}
                 </div>
-                <span className="text-xs text-gray-600">
+                <span
+                  className={cn(
+                    'text-xs text-gray-600 min-w-[60px] text-center',
+                    {
+                      'text-green-500': copiedLink,
+                    }
+                  )}
+                >
                   {copiedLink ? 'Copied!' : 'Copy link'}
                 </span>
               </button>
             </div>
 
-            <div className="flex flex-col items-center gap-1">
+            <div className="flex flex-col items-center gap-1 w-16">
               <FacebookShareButton title={link.title!} url={link.url!}>
                 <FacebookIcon className="w-12 h-12 rounded-full" />
               </FacebookShareButton>
-              <span className="text-xs text-gray-600">Facebook</span>
+              <span className="text-xs text-gray-600 min-w-[60px] text-center">
+                Facebook
+              </span>
             </div>
 
-            <div className="flex flex-col items-center gap-1">
+            <div className="flex flex-col items-center gap-1 w-16">
               <TwitterShareButton title={link.title!} url={link.url!}>
                 <TwitterIcon className="w-12 h-12 rounded-full" />
               </TwitterShareButton>
-              <span className="text-xs text-gray-600">Twitter</span>
+              <span className="text-xs text-gray-600 min-w-[60px] text-center">
+                Twitter
+              </span>
             </div>
 
-            <div className="flex flex-col items-center gap-1">
+            <div className="flex flex-col items-center gap-1 w-16">
               <WhatsappShareButton title={link.title!} url={link.url!}>
                 <WhatsappIcon className="w-12 h-12 rounded-full" />
               </WhatsappShareButton>
-              <span className="text-xs text-gray-600">WhatsApp</span>
+              <span className="text-xs text-gray-600 min-w-[60px] text-center">
+                WhatsApp
+              </span>
             </div>
 
-            <div className="flex flex-col items-center gap-1">
+            <div className="flex flex-col items-center gap-1 w-16">
               <LinkedinShareButton title={link.title!} url={link.url!}>
                 <LinkedinIcon className="w-12 h-12 rounded-full" />
               </LinkedinShareButton>
-              <span className="text-xs text-gray-600">LinkedIn</span>
+              <span className="text-xs text-gray-600 min-w-[60px] text-center">
+                LinkedIn
+              </span>
             </div>
 
-            <div className="flex flex-col items-center gap-1">
+            <div className="flex flex-col items-center gap-1 w-16">
               <TelegramShareButton title={link.title!} url={link.url!}>
                 <TelegramIcon className="w-12 h-12 rounded-full" />
               </TelegramShareButton>
-              <span className="text-xs text-gray-600">Telegram</span>
+              <span className="text-xs text-gray-600 min-w-[60px] text-center">
+                Telegram
+              </span>
             </div>
 
-            <div className="flex flex-col items-center gap-1">
+            <div className="flex flex-col items-center gap-1 w-16">
               <EmailShareButton title={link.title!} url={link.url!}>
                 <EmailIcon className="w-12 h-12 rounded-full" />
               </EmailShareButton>
-              <span className="text-xs text-gray-600">Email</span>
+              <span className="text-xs text-gray-600 min-w-[60px] text-center">
+                Email
+              </span>
             </div>
 
             {isClient && (
               <div className="flex-shrink-0">
                 <button
                   onClick={handleNativeShare}
-                  className="flex flex-col items-center gap-1"
+                  className="flex flex-col items-center gap-1 w-16"
                 >
                   <div className="w-12 h-12 rounded-full bg-gray-100 hover:bg-gray-200 flex items-center justify-center">
-                    <MoreHorizontal className="w-5 h-5" />
+                    <Share className="w-5 h-5" />
                   </div>
-                  <span className="text-xs text-gray-600">More</span>
+                  <span className="text-xs text-gray-600 min-w-[60px] text-center">
+                    More
+                  </span>
                 </button>
               </div>
             )}
