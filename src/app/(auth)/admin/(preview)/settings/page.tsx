b3ff7c7c7c7ab9632d5a8093cbbed58677c1d41a
Jordy Morales <dev.morales.jordy@gmail.com>
2025-02-25 07:57:34 -0400

    feat: add SettingsPage component and rename AdminAnalyticsPage to AnalyticsPage

--- src/app/(auth)/admin/(preview)/settings/page.tsx
+++ src/app/(auth)/admin/(preview)/settings/page.tsx
@@ -1,7 +1,7 @@
-export default function AnalyticsPage() {
+export default function SettingsPage() {
   return (
     <div className="flex flex-col items-start gap-4 px-2 py-8">
-      <h2 className="text-3xl font-bold lg:text-5xl">Analytics</h2>
+      <h2 className="text-3xl font-bold lg:text-5xl">Settings</h2>
       <p className="text-muted-foreground">Coming soon...</p>
     </div>
   );
