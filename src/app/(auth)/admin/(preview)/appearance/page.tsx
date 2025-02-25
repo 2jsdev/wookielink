876ce79d85d58020314a8b5b0f35f026ccaa0856
Jordy Morales <dev.morales.jordy@gmail.com>
2025-02-25 07:57:21 -0400

    fix: comment out ThemeSelector component in AppearancePage for temporary removal

--- src/app/(auth)/admin/(preview)/appearance/page.tsx
+++ src/app/(auth)/admin/(preview)/appearance/page.tsx
@@ -1,4 +1,4 @@
-import ThemeSelector from '@/components/custom/Customizer/ThemeSelector';
+// import ThemeSelector from '@/components/custom/Customizer/ThemeSelector';
 import CustomAppearance from '@/components/custom/CustomAppearance';
 import ButtonAppearance from '@/components/custom/ButtonAppearance';
 import FontAppearance from '@/components/custom/FontAppearance';
@@ -6,7 +6,7 @@ import FontAppearance from '@/components/custom/FontAppearance';
 export default function AppearancePage() {
   return (
     <>
-      <ThemeSelector />
+      {/* <ThemeSelector /> */}
       <CustomAppearance />
       <ButtonAppearance />
       <FontAppearance />
